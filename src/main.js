83f6b6df3f3c9f5b220550749dfb0a95dbfcc56d
Sojourner1066 <morgangrundy@gmail.com>
2025-04-12 14:48:55 +0100

    added svg icons

--- src/main.js
+++ src/main.js
@@ -1,13 +1,15 @@
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
 import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
+import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer";
+import PictureMarkerSymbol from "@arcgis/core/symbols/PictureMarkerSymbol";
 
-// Create map
+// Create the map
 const map = new Map({
   basemap: "topo-vector"
 });
 
-// Create view
+// Create the view
 const view = new MapView({
   container: "viewDiv",
   map: map,
@@ -15,50 +17,82 @@ const view = new MapView({
   zoom: 11
 });
 
-// Viewing Locations
-const viewingLocationsLayer = new FeatureLayer({
-  url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/ViewingLocations/FeatureServer/0",
-  title: "Viewing Locations",
-  outFields: ["*"],
-  popupTemplate: {
-    title: "{name}",
-    content: "{site_info}"
-  }
+// --- Camping Locations ---
+const campingRenderer = new SimpleRenderer({
+  symbol: new PictureMarkerSymbol({
+    url: "/camping.svg",
+    width: "24px",
+    height: "24px"
+  })
 });
-map.add(viewingLocationsLayer);
 
-// Parking Locations
-const parkingLocationsLayer = new FeatureLayer({
-  url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/ParkingLocations/FeatureServer/0",
-  title: "Parking Locations",
-  outFields: ["*"],
-  popupTemplate: {
-    title: "{name}",
-    content: "{site_info}"
-  }
-});
-map.add(parkingLocationsLayer);
-
-// Camping Locations
-const campingLocationsLayer = new FeatureLayer({
+const campingLayer = new FeatureLayer({
   url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/CampingLocations/FeatureServer/0",
-  title: "Camping Locations",
+  renderer: campingRenderer,
+  outFields: ["*"],
+  popupTemplate: {
+    title: "{name}",
+    content: "{facilities}"
+  }
+});
+map.add(campingLayer);
+
+// --- Parking Locations ---
+const parkingRenderer = new SimpleRenderer({
+  symbol: new PictureMarkerSymbol({
+    url: "/parking.svg",
+    width: "24px",
+    height: "24px"
+  })
+});
+
+const parkingLayer = new FeatureLayer({
+  url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/ParkingLocations/FeatureServer/0",
+  renderer: parkingRenderer,
+  outFields: ["*"],
+  popupTemplate: {
+    title: "{name}",
+    content: "{spots}"
+  }
+});
+map.add(parkingLayer);
+
+// --- Viewing Locations ---
+const viewRenderer = new SimpleRenderer({
+  symbol: new PictureMarkerSymbol({
+    url: "/view.svg",
+    width: "24px",
+    height: "24px"
+  })
+});
+
+const viewingLayer = new FeatureLayer({
+  url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/ViewingLocations/FeatureServer/0",
+  renderer: viewRenderer,
   outFields: ["*"],
   popupTemplate: {
     title: "{name}",
     content: "{site_info}"
   }
 });
-map.add(campingLocationsLayer);
+map.add(viewingLayer);
 
-// Flight Route (likely a polyline)
-const flightRouteLayer = new FeatureLayer({
-  url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/FlightRoute/FeatureServer/0",
-  title: "Flight Route",
+// --- RAF Bases ---
+const rafRenderer = new SimpleRenderer({
+  symbol: new PictureMarkerSymbol({
+    url: "/fighter-jet.svg",
+    width: "24px",
+    height: "24px"
+  })
+});
+
+const rafLayer = new FeatureLayer({
+  url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/RAF_Bases/FeatureServer/0",
+  renderer: rafRenderer,
   outFields: ["*"],
   popupTemplate: {
-    title: "Flight Route",
-    content: "Low flying jet route"
+    title: "{name}",
+    content: "{aircraft}"
   }
 });
-map.add(flightRouteLayer);
+map.add(rafLayer);
