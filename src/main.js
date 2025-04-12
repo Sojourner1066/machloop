30236fa1fd96122a002e0a9ed9ffd3bb922bcf1b
Sojourner1066 <morgangrundy@gmail.com>
2025-04-12 14:16:41 +0100

    adding layers

--- src/main.js
+++ src/main.js
@@ -1,13 +1,64 @@
 import Map from "@arcgis/core/Map";
 import MapView from "@arcgis/core/views/MapView";
+import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
 
+// Create map
 const map = new Map({
   basemap: "topo-vector"
 });
 
+// Create view
 const view = new MapView({
   container: "viewDiv",
   map: map,
-  center: [-122.4194, 37.7749], // San Francisco
-  zoom: 12
+  center: [-3.771882, 52.687300],
+  zoom: 11
 });
+
+// Viewing Locations
+const viewingLocationsLayer = new FeatureLayer({
+  url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/ViewingLocations/FeatureServer/0",
+  title: "Viewing Locations",
+  outFields: ["*"],
+  popupTemplate: {
+    title: "{name}",
+    content: "{site_info}"
+  }
+});
+map.add(viewingLocationsLayer);
+
+// Parking Locations
+const parkingLocationsLayer = new FeatureLayer({
+  url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/ParkingLocations/FeatureServer/0",
+  title: "Parking Locations",
+  outFields: ["*"],
+  popupTemplate: {
+    title: "{name}",
+    content: "{site_info}"
+  }
+});
+map.add(parkingLocationsLayer);
+
+// Camping Locations
+const campingLocationsLayer = new FeatureLayer({
+  url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/CampingLocations/FeatureServer/0",
+  title: "Camping Locations",
+  outFields: ["*"],
+  popupTemplate: {
+    title: "{name}",
+    content: "{site_info}"
+  }
+});
+map.add(campingLocationsLayer);
+
+// Flight Route (likely a polyline)
+const flightRouteLayer = new FeatureLayer({
+  url: "https://services.arcgis.com/HRPe58bUyBqyyiCt/arcgis/rest/services/FlightRoute/FeatureServer/0",
+  title: "Flight Route",
+  outFields: ["*"],
+  popupTemplate: {
+    title: "Flight Route",
+    content: "Low flying jet route"
+  }
+});
+map.add(flightRouteLayer);
